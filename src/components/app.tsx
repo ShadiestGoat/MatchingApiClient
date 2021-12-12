f9f8f02dd4ed9a3ec25a46b3efb3651c1a70155c
ShadiestGoat <shady@shadygoat.eu>
2021-12-12 00:48:54 +0000

    Version change

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,7 +6,7 @@ import { allQuestions, question, questions } from './questionair';
 import { questionsHere } from './additionalQs';
 import axios from 'axios';
 
-const V = "1.0.2"
+const V = "1.1.0"
 
 const App: FunctionalComponent = () => {
     if (localStorage.getItem("v") != V) {
